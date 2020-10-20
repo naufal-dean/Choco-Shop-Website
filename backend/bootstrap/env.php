07b1e698a8b2a4559dacf0a864b18488c3a365a5
naufal-dean <naufal.dean@gmail.com>
2020-10-20 15:02:04 +0700

    Remove env.php from git

--- backend/bootstrap/env.php
+++ backend/bootstrap/env.php
@@ -1,14 +1,1 @@
 <?php
-
-$envs = [
-    'APP_NAME' => 'WillyWangkyWebsite',
-
-    'DB_HOST' => '127.0.0.1',
-    'DB_PORT' => '3306',
-    'DB_DATABASE' => 'mysql_database_name',
-    'DB_USERNAME' => 'mysql_username',
-    'DB_PASSWORD' => 'mysql_password',
-];
-
-foreach ($envs as $key => $value)
-    putenv(sprintf('%s=%s', $key, $value));
