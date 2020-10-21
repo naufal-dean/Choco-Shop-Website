754372715fe8bf3d053a0c6d3b7616ad6beae81d
Rizky I. F <rykzius@gmail.com>
2020-10-21 22:33:56 +0700

    Add foreign key to user in transaction table

--- backend/database/migrations/CreateTransactionTable.php
+++ backend/database/migrations/CreateTransactionTable.php
@@ -8,13 +8,15 @@ class CreateTransactionTable
         $conn->query("
         CREATE TABLE IF NOT EXISTS transaction (
             id int PRIMARY KEY AUTO_INCREMENT,
+            user_id int,
             chocolate int,
             amount int,
             total_price bigint,
             address varchar(20),
             transaction_date date,
             transaction_time time,
-            FOREIGN KEY (chocolate) REFERENCES chocolate(id)
+            FOREIGN KEY (chocolate) REFERENCES chocolate(id),
+            FOREIGN KEY (user_id) REFERENCES user(id)
         );");
         $conn->commit();
         echo '[+] Done...'.PHP_EOL;
