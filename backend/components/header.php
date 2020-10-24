cd9b5f3bc1cfb8d251e25c6c033d7467112ddc4b
Rizky I. F <rykzius@gmail.com>
2020-10-24 20:08:26 +0700

    Added error handling on header

--- backend/components/header.php
+++ backend/components/header.php
@@ -4,15 +4,17 @@
             $nav_temp_var_1 = '';
             $nav_temp_var_2 = '';
             $nav_temp_var_3 = '';
-            if ($nav_page == 'dashboard') {
-                $nav_temp_var_1 = ' nav-active';
-            } elseif ($nav_page == 'add_chocolate') {
-                $nav_temp_var_2 = ' nav-active';
-            } elseif ($nav_page == 'transaction_history') {
-                $nav_temp_var_3 = ' nav-active';
+            if (isset($nav_page)) {
+                if ($nav_page == 'dashboard') {
+                    $nav_temp_var_1 = ' nav-active';
+                } elseif ($nav_page == 'add_chocolate') {
+                    $nav_temp_var_2 = ' nav-active';
+                } elseif ($nav_page == 'transaction_history') {
+                    $nav_temp_var_3 = ' nav-active';
+                }
             }
             echo '<a class="nav-menu'.$nav_temp_var_1.'"'.($nav_temp_var_1 ? '' : ' href="/"').'>Home</a>';
-            if ($user_info['is_superuser']) {
+            if (isset($user_info) && $user_info['is_superuser']) {
                 echo '<a class="nav-menu'.$nav_temp_var_2.'"'.($nav_temp_var_2 ? '' : ' href="/add_chocolate"').'>Add 🍫</a>';
             } else {
                 echo '<a class="nav-menu'.$nav_temp_var_3.'"'.($nav_temp_var_3 ? '' : ' href="/transaction_history"').'>History</a>';
@@ -21,9 +23,9 @@
     </div>
     <div id="nav-middle-container" class="nav-menus">
         <div id="nav-hamburger" class="nav-menu nav-mobile-only" onclick="toggleMenu()">
-            <img id="nav-hamburger-img" src="static/images/hamburger.svg" alt="hamburger">
+            <img id="nav-hamburger-img" src="/static/images/hamburger.svg" alt="hamburger">
         </div>
-        <form id="nav-search-form">
+        <form id="nav-search-form" action="/search/" method="POST">
             <span id="nav-search-logo">🔍</span>
             <input type="text" name="search" id="nav-search">
         </form>
