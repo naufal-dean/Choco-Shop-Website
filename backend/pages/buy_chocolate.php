67468f6e4cc72d8e1a33b7a5f4d226c290a7b7bb
naufal-dean <naufal.dean@gmail.com>
2020-10-25 06:58:47 +0700

    Improve buy chocolate page ui

--- backend/pages/buy_chocolate.php
+++ backend/pages/buy_chocolate.php
@@ -3,7 +3,7 @@
     <head>
         <title>Buy - Choco Shop</title>
         <link rel="stylesheet" href="/static/css/header.css">
-        <link rel="stylesheet" href="/static/css/dashboard.css">
+        <link rel="stylesheet" href="/static/css/detail_chocolate.css">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
 <body>
@@ -22,40 +22,43 @@
                             </div>
                             <div class="chocolate-detail-desc">
                                 <ul>
-                                    <li>Amount sold: '.$chocolate['sold'].'</li>
-                                    <li>Price: '.$chocolate['price'].'</li>
-                                    <li>Amount: '.$chocolate['stock'].'</li>
-                                    <li>Description: '.$chocolate['description'].'</li>
+                                    <li><span>Amount sold:</span> '.$chocolate['sold'].'</li>
+                                    <li><span>Price:</span> Rp '.$chocolate['price'].',00</li>
+                                    <li><span>Amount:</span> '.$chocolate['stock'].'</li>
+                                    <li><span>Description:</span> '.$chocolate['description'].'</li>
                                     <li>
                                         <ul>
-                                            <li>Amount to Buy:</li>
-                                            <li>
+                                            <li><span>Amount to Buy:</span></li>
+                                            <li class="chocolate-detail-amount">
+                                                <div>
+                                                    <input id="dec-amount" type="button" onclick="decAmount()" value="-">
+                                                </div>
                                                 <div>
                                                     <input class="amount-input" type="text" id="amount" name="amount" required>
                                                 </div>
                                                 <div>
                                                     <input id="inc-amount" type="button" onclick="incAmount()" value="+">
                                                 </div>
-                                                <div>
-                                                    <input id="dec-amount" type="button" onclick="decAmount()" value="-">
-                                                </div>
                                             </li>
                                         </ul>
                                     </li>
                                 </ul>
                             </div>
-                            <div class="chocolate-detail-option">
-                                <label class="form-input-label" for="address">Address:</label><br>
-                                <textarea class="form-input-box" id="address" name="address" form="buy-form" required></textarea><br>
-                                <input class="form-input-submit" type="button" onclick="goBack()" value="Cancel">
-                                <input class="form-input-submit" type="submit" value="Add">
-                            </div>
+                        </div>
+                        <div class="chocolate-detail-address">
+                            <label class="form-input-label" for="address"><span>Address:</span></label><br>
+                            <textarea class="form-input-box" id="address" name="address" form="buy-form" required></textarea><br>
+                        </div>
+                        <div class="chocolate-detail-option">
+                            <input class="form-input-submit chocolate-detail-btn" type="button" onclick="goBack()" value="Cancel">
+                            <input class="form-input-submit chocolate-detail-btn" type="submit" value="Buy">
                         </div>
                     </form>
                 ';
             ?>
         </div>
     </div>
+    <script src="/static/js/helper.js"></script>
     <script src="/static/js/responsive.js"></script>
     <script src="/static/js/buy_chocolate.js"></script>
     </body>
