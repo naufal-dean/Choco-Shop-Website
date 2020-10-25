bde2745a4e28b9bffe676c21f069dec438a6b775
Yonatan <48679462+haverzard@users.noreply.github.com>
2020-10-25 12:51:26 +0700

    Fix id regex on api route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -6,17 +6,17 @@ Router::post('/api/users/login/', 'UserController@login');
 Router::post('/api/users/logout/', 'UserController@logout');
 Router::post('/api/users/register/', 'UserController@register');
 Router::get('/api/users/logout', 'UserController@logout');
-Router::get('/api/users/([1-9]+)/', 'UserController@id_lookup');
+Router::get('/api/users/([0-9]+)/', 'UserController@id_lookup');
 Router::get('/api/users/username_lookup/', 'UserController@username_lookup');
 Router::get('/api/users/email_lookup/', 'UserController@email_lookup');
 
 # Chocolate Api
 Router::get('/api/chocolates/', 'ChocolateController@show_top_selling_chocolates');
-Router::get('/api/chocolates/([1-9]+)/', 'ChocolateController@id_lookup');
+Router::get('/api/chocolates/([0-9]+)/', 'ChocolateController@id_lookup');
 Router::get('/api/chocolates/find/', 'ChocolateController@find_chocolates');
 Router::post('/api/chocolates/add/', 'ChocolateController@add_chocolate');
-Router::put('/api/chocolates/([1-9]+)/add', 'ChocolateController@add_stock_chocolate');
-Router::put('/api/chocolates/([1-9]+)/buy', 'ChocolateController@buy_chocolate');
+Router::put('/api/chocolates/([0-9]+)/add', 'ChocolateController@add_stock_chocolate');
+Router::put('/api/chocolates/([0-9]+)/buy', 'ChocolateController@buy_chocolate');
 
 # Transactions Api
 Router::get('/api/transactions/', 'TransactionController@get_user_transactions');
@@ -29,6 +29,6 @@ Router::get('/', 'PageController@dashboard_page');
 Router::get('/add_chocolate/', 'PageController@add_chocolate_page');
 Router::get('/transaction_history/', 'PageController@transaction_history_page');
 Router::get('/search/', 'PageController@search_page');
-Router::get('/detail_chocolate/([1-9]*)/', 'PageController@detail_chocolate_page');
-Router::get('/detail_chocolate/([1-9]*)/add/', 'PageController@add_stock_chocolate_page');
-Router::get('/detail_chocolate/([1-9]*)/buy/', 'PageController@buy_chocolate_page');
+Router::get('/detail_chocolate/([0-9]*)/', 'PageController@detail_chocolate_page');
+Router::get('/detail_chocolate/([0-9]*)/add/', 'PageController@add_stock_chocolate_page');
+Router::get('/detail_chocolate/([0-9]*)/buy/', 'PageController@buy_chocolate_page');
